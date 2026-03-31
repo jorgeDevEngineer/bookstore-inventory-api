8fd33316eef383bc0e1dac4276771be90a6c910d
Jorge Ramírez <jorguitoramirez@gmail.com>
2026-03-31 14:32:31 -0400

    build(core): configure rest framework pagination and main url routing

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # <-- Asegúrate de que 'include' esté importado
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Aquí estamos diciendo: "Toda ruta que empiece con 'api/' mándala a inventory.urls"
     path('api/', include('inventory.urls')), 
 ]
